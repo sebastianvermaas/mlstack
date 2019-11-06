8057e9208fd1b9309f45a01c6cf53f40c091773c
sebastian vermaas <sebastian.vermaas@gmail.com>
2019-11-06 22:55:33 +0100

    black code

--- mlstack/clients/docker.py
+++ mlstack/clients/docker.py
@@ -13,23 +13,6 @@ class DockerClient(docker.APIClient):
         """ Initializes a DockerClient for building and pulling images """
         super().__init__()
 
-    def pull_images(self, repositories: list):
-        """
-        Pulls images from Docker repositories
-        """
-        ids = list()
-        for repository in repositories:
-            logger.info("Pulling from %s\n", repository)
-            for i, line in enumerate(self.pull(repository, stream=True, decode=True)):
-                if line.get("id", None):
-                    ids.append(line.get("id"))
-                    ids = list(set(ids))
-                for id in ids:
-                    if "." not in id:
-                        if line.get("progressDetail", None):
-                            if i % 15== 0:
-                                logger.info("{id}: {status} {progress}".format(id=id, status=line.get("status"), progress=line.get("progress")))
-
     def build_images(self, images: list):
         """
         Builds Docker images from a Dockerfile.
@@ -58,3 +41,26 @@ class DockerClient(docker.APIClient):
                     logger.warning(line)
 
             logger.info("Woohoo! Successfully built %s", image)
+
+    def pull_images(self, repositories: list):
+        """
+        Pulls images from Docker repositories
+        """
+        ids = list()
+        for repository in repositories:
+            logger.info("Pulling from %s\n", repository)
+            for i, line in enumerate(self.pull(repository, stream=True, decode=True)):
+                if line.get("id", None):
+                    ids.append(line.get("id"))
+                    ids = list(set(ids))
+                for id in ids:
+                    if "." not in id:
+                        if line.get("progressDetail", None):
+                            if i % 15 == 0:
+                                logger.info(
+                                    "{id}: {status} {progress}".format(
+                                        id=id,
+                                        status=line.get("status"),
+                                        progress=line.get("progress"),
+                                    )
+                                )
