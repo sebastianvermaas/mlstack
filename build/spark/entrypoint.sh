1ebe3e2406c7806f78fa58a1e291c605b9ae8082
Sebastian Vermaas <sebastian.vermaas@nike.com>
2019-11-08 15:41:16 +0100

    spark initial deployment working

--- build/spark/entrypoint.sh
+++ build/spark/entrypoint.sh
@@ -11,16 +11,21 @@ case "${SPARK_ROLE}" in
     exec "${SPARK_HOME}/bin/spark-class" org.apache.spark.deploy.master.Master
     ;;
   slave)
+    echo 'Starting Spark slave'
     exec "${SPARK_HOME}/bin/spark-class" org.apache.spark.deploy.worker.Worker "${SPARK_MASTER_HOST:-spark-master}:${SPARK_MASTER_PORT:-7077}"
     ;;
   history)
     mkdir -p "${SPARK_HOME}/logs/spark-events"
+    echo 'Starting Spark history server'
     exec "${SPARK_HOME}/bin/spark-class" org.apache.spark.deploy.history.HistoryServer
     ;;
   edge)
+    echo 'Starting edge spark node'
     sleep infinity
     ;;
   *)
     abort "Unsupported role ${SPARK_ROLE}"
     ;;
 esac
+
+
